21694d7061178965a7efcb89924aa32a3a1f69bb
alvinto414 <apt6@sfu.ca>
2014-03-24 19:57:55 -0700

    added function to auto hide menu list when clicked. added smooth scrolling function.

--- code/js/ui.js
+++ code/js/ui.js
@@ -2,13 +2,32 @@ $(document).ready(function(){
 
 	var windowWidth = $(window).width();
 
+	$('a[href*=#]').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+	    && location.hostname == this.hostname) {
+	      var $target = $(this.hash);
+	      $target = $target.length && $target
+	      || $('[name=' + this.hash.slice(1) +']');
+	      if ($target.length) {
+	        var targetOffset = $target.offset().top;
+	        $('html,body')
+	        .animate({scrollTop: targetOffset}, 500);
+	       return false;
+	      }
+	    }
+  	});
+
 	if(windowWidth < 480){
 		$("#navToggle").css("display","block");
 		$("nav").hide();
 	}
 
 	$("#navToggle").click(function() {
-		$("nav").slideToggle(1000);
+		$("nav").slideToggle(750);
+	});
+
+	$(".main-menu").click(function() {
+		$("nav").slideToggle(750);
 	});
 
 	$(window).resize(function(){
