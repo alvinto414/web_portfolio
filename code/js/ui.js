d086981e461725dcf7c0a890044df6149e15c92e
alvinto414 <apt6@sfu.ca>
2014-06-21 15:21:39 -0700

    added code to scroll up to top

--- code/js/ui.js
+++ code/js/ui.js
@@ -43,4 +43,19 @@ $(document).ready(function(){
 			$("nav").hide();
 		}
 	});
+
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
